89f6666a5bbac39e4847ee1bec3484f1bd6312f5
Aleisha Mork <aleisha.mork@gmail.com>
2023-03-02 19:25:46 -0700

    Completed Medusa Creature with Person and Statue classes

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,20 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+
+  gazeAtVictim(victim) {
+    var statue = new Statue(victim.name)
+    this.statues.push(statue)
+    if(this.statues.length > 3){
+      var person = this.statues.shift()
+      return new Person(person.name, 'relieved')
+    }
+  }
+}
+
+module.exports = Medusa;
